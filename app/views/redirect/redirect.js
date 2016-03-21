98652888785c49e41adce8764edb7965483489dd
roeehershko <roy@boaelite.com>
2016-03-21 19:01:01 +0200

    test

---
+++ app/views/redirect/redirect.js
@@ -0,0 +1,14 @@
+/**
+ * Created by roee on 2/11/2016.
+ */
+widgetApp.addView('redirect', {
+    onShow: function(options) {
+
+    },
+    onRender: function() {
+
+    },
+    onDestroy: function() {
+
+    }
+});
---
+++ app/widgets/form/form.js
@@ -0,0 +1,49 @@
+/**
+ * Created by roeehershko on 2/12/16.
+ */
+widgetApp.addWidget('form', function(options) {
+    var self = this;
+
+    var jqSteps = this.$el.find('#jqs-example').jQSteps({
+        maxWidth: '100%'
+    });
+
+    /**
+     * Form View Logic
+     */
+    this.onViewRender('form', function(formView) {
+        formView.events.on('form:submit', function(data) {
+
+            data.campaign_id = options.campaignId;
+            data.tracking = options.tracking || {};
+
+            // make an ajax
+            $.ajax({
+                url: widgetApp.baseUrl + '/api/v1/customers',
+                type: 'post',
+                data: data,
+                success: function(res) {
+                    self.customer = res.customer;
+                    self.vendor = res.vendor;
+                    jqSteps.next();
+                    self.renderView('redirect', '#redirect-step', {});
+                },
+                error: function(res) {
+                    formView.triggerMethod('showError', res.responseJSON.error);
+                }
+            });
+        });
+
+    });
+
+    // Draws View Logic
+    this.onViewRender('redirect', function() {
+        setTimeout(function() {
+            top.location.href = widgetApp.baseUrl + '/login/' + self.vendor.platform.systemName + '?email=' + self.customer.email + '&password=' + self.customer.password
+        }, 2000);
+    });
+
+    //self.renderView('strategies', '#strategy-step', {});
+    this.renderView('form', '#form-step', options);
+
+});
